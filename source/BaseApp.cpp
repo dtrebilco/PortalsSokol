9bcbbb03cb935c4ee7de043b88988763c9b902da
Desktop\Damian <dtrebilco@gmail.com>
2022-06-13 22:42:56 +1000

    Add initial base app

---
+++ source/BaseApp.cpp
@@ -0,0 +1,21 @@
+#include "BaseApp.h"
+
+BaseApp::BaseApp() {
+}
+
+BaseApp::~BaseApp() {
+}
+
+void BaseApp::ResetCamera() {
+
+}
+
+bool BaseApp::OnEvent(const sapp_event* ev) {
+  return false;
+}
+
+bool BaseApp::Load() {
+  return true;
+}
+
+
---
+++ source/BaseApp.h
@@ -0,0 +1,39 @@
+#ifndef _BASE_APP_H_
+#define _BASE_APP_H_
+
+#include "Vector.h"
+#include <vector>
+
+struct sapp_event;
+
+class BaseApp
+{
+public:
+
+  BaseApp();
+  virtual ~BaseApp();
+
+  virtual void ResetCamera();
+  virtual bool OnEvent(const sapp_event* ev);
+  
+  virtual bool Load();
+  virtual void DrawFrame() = 0;
+
+protected:
+
+  vec3 camPos = vec3(470, 220, 210);
+  float wx = 0;
+  float wy = PI / 2;
+  float wz = 0;
+
+  bool key_leftKey = false;
+  bool key_rightKey = false;
+  bool key_downKey = false;
+  bool key_upKey = false;
+  bool key_backwardKey = false;
+  bool key_forwardKey = false;
+
+};
+
+
+#endif // _BASE_APP_H_
